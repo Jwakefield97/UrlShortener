6ade0acbca76af3854a91a679cd4f0926e0480a5
Jwakefield97 <jacobstevenwakefield@gmail.com>
2019-08-10 16:28:58 -0500

    add endpoint for getting currently mapped urls

--- server.go
+++ server.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"net/http"
-	"sync"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +15,8 @@ func hash(s string) string {
 }
 
 func main() {
-	var urls sync.Map
+	urls := map[string]string{}
+	urls["1"] = "https://www.google.com/search?q=images&rlz=1C1CHBF_enUS796US796&tbm=isch&source=iu&ictx=1&fir=8c_UAo3gH_220M%253A%252CShwNVOdFBcmkxM%252C_&vet=1&usg=AI4_-kQpJbM15TX5VhVDvz1rgddeX4UTow&sa=X&ved=2ahUKEwj8sIK0lfnjAhUWCs0KHU6MBUsQ9QEwAXoECAYQBg#imgrc=8c_UAo3gH_220M:"
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 	r.StaticFS("/resources", http.Dir("resources"))
@@ -25,19 +25,26 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
+	//return the map of currently mapped hashs to urls
+	r.GET("/urls", func(c *gin.Context) {
+		c.JSON(200, urls)
+	})
+
+	//add a new hash to the url
 	r.POST("/new", func(c *gin.Context) {
 		newURL := c.PostForm("url")
 		urlHash := hash(newURL)
-		urls.Store(urlHash, newURL)
+		urls[urlHash] = newURL
 		c.JSON(200, gin.H{
 			"hash": urlHash,
 		})
 	})
 
+	//redirect to mapped url
 	r.GET("/h/:hash", func(c *gin.Context) {
-		url, ok := urls.Load(c.Param("hash"))
+		url, ok := urls[c.Param("hash")]
 		if ok {
-			c.Redirect(http.StatusFound, url.(string))
+			c.Redirect(http.StatusFound, url)
 		} else {
 			c.Status(http.StatusNotFound)
 		}
